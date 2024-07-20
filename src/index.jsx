7efb52e2736f3fe286b173167ea2e16aed4e37cb
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-07-20 23:58:43 +0530

    test

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,9 @@ import { ToastContainer, toast } from 'react-toastify'; // Import from react-toa
 import 'react-toastify/dist/ReactToastify.css'; // Import the CSS for styling
 import App from './App';
 import { AuthProvider } from './Authentication/authContext';
-// src/index.js or src/App.js
 import './index.css'
 
-// Use createRoot from ReactDOM/client
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// Render your app within createRoot, wrapped with ToastContainer
 root.render(
   <React.StrictMode>
     <AuthProvider>
